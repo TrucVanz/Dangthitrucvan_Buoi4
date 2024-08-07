f003bd40c1bc9bbaf655be2c4e328b03cb523d73
TrucVanz <Trucvan3012@gmail.com>
2024-08-07 14:10:40 +0700

    Code cau 1.2 + HinhAnh KQ va Debug

---
+++ BTTL_Buoi4/BTTL_Buoi4/Cau01.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy tính tổng phân số
+double tongPhanSo(int n) {
+	if (n == 1) {
+		return 1.0 / 2;  // Cơ sở của đệ quy
+	}
+	else {
+		return n / (double)(n + 1) + tongPhanSo(n - 1);  // Đệ quy
+	}
+}
+
+int main() {
+	int n;
+	printf("Nhap mot so nguyen duong: ");
+	scanf_s("%d", &n);
+
+	if (n < 1) {
+		printf("Nhap n >= 1.\n");
+	}
+	else {
+		printf("Tong S(%d) = %.4f\n", n, tongPhanSo(n));
+	}
+	getchar();
+	return 0;
+}

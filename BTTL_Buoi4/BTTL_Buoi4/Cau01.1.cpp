04fb4a95b9f203c0d913e78283afaa0e7ea501db
TrucVanz <Trucvan3012@gmail.com>
2024-08-07 13:57:38 +0700

    Code cau 1 + HinhAnh KQ va Debug

---
+++ BTTL_Buoi4/BTTL_Buoi4/Cau01.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy tính tổng của các số từ 1 đến n
+int tong(int n) {
+	if (n <= 0) {
+		return 0;  // Cơ sở của đệ quy
+	}
+	else {
+		return n + tong(n - 1);  // Đệ quy
+	}
+}
+
+int main() {
+	int n;
+	printf("Nhap mot so nguyen duong: ");
+	scanf_s("%d", &n);
+
+	if (n < 0) {
+		printf("Khong tinh tong cho so am.\n");
+	}
+	else {
+		printf("Tong S(%d) = %d\n", n, tong(n));
+	}
+
+	return 0;
+}
